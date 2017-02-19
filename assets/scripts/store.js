4049f072c14533583ed134aa69210d358833862c
Bowers96 <bowersc96@gmail.com>
2017-02-18 20:44:10 -0500

    clean-up for new project

--- assets/scripts/store.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const store = {
-  host: 'http://localhost:4741/',
-};
-
-window.store = store;
-
-module.exports = store;
--- spec/example-spec.js
+++ spec/example-spec.js
@@ -2,8 +2,8 @@
 
 const example = require('../assets/scripts/example');
 
-describe('Example', function () {
-  it('is true', function () {
+describe('Example', function() {
+  it('is true', function() {
     expect(example).toBe(true);
   });
 });
